480143fd0386beb6d727033799c68b9052a7811e
Mohammad Norouzi <mnorouzidev@gmail.com>
2023-10-05 10:42:42 +0330

    fix: add todu.ir for allowed origins in cors

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
-app.options('*', cors());
+app.use(cors({
+    origin: 'https://todu.ir'
+}));
+app.options('*', cors({ origin: 'https://todu.ir' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
